8949e6f0d23bcd26cb0b38da4641d163b317923e
Jose Santos <josesantosiglesias@gmail.com>
2023-06-17 16:36:20 +0200

    Segundo print "Comenzamos"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome to book´N go!");
+        System.out.printf("Comenzamos");
 
         }
     }
